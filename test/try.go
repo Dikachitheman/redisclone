37eb84cbcf6ff8436988f24527d49604d579e2f8
dikachitheman <dikachi.anosike@gmail.com>
2024-01-26 01:48:51 +0100

    testing

--- test/try.go
+++ test/try.go
@@ -1,139 +1,83 @@
 package main
+
 import (
-	"bufio"
-	"bytes"
+	// "errors"
 	"fmt"
-	"io"
+	// "io"
 	"log"
 	"net"
+	"os"
+	"runtime"
 	"strconv"
 	"strings"
 )
-//*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n
-const (
-	Array = '*'
-	Bulk  = '$'
-)
-func decode(r *bufio.Reader) ([]string, error) {
-	b, err := r.ReadByte()
-	if err != nil {
-		return nil, err
-	}
-	switch b {
-	case Bulk:
-		vbarr, err := validBytes(r)
-		if err != nil {
-			return nil, err
-		}
-		count, err := strconv.Atoi(string(vbarr))
-		if err != nil {
-			return nil, err
-		}
-		buf := make([]byte, count+2)
-		_, err = io.ReadFull(r, buf)
-		if err != nil && err != io.EOF {
-			return nil, err
-		}
-		return []string{string(bytes.TrimSpace(buf))}, nil
-	default:
-		return nil, fmt.Errorf("could not decode the stream")
-	}
-}
-func validBytes(r *bufio.Reader) ([]byte, error) {
-	barr, err := r.ReadBytes('\n')
-	if err != nil {
-		return nil, err
-	}
-	return barr[:len(barr)-2], nil
-}
-func decodeArray(r *bufio.Reader) ([]string, error) {
-	vbarr, err := validBytes(r)
-	if err != nil {
-		return nil, err
-	}
-	l, err := strconv.Atoi(string(vbarr))
-	if err != nil {
-		return nil, err
-	}
-	cmds := []string{}
-	for i := 0; i < l; i++ {
-		vals, err := decode(r)
-		if err == io.EOF {
-			return vals, nil
-		}
-		if err != nil {
-			return nil, err
-		}
-		cmds = append(cmds, vals...)
-	}
-	return cmds, nil
-}
 
-type input struct {
-	raw  []byte
-	cmds []string
-}
+func goid() int {
 
+	buf := make([]byte, 32)
+	n := runtime.Stack(buf[:], false)
 
-func (i *input) parse() {
-	r := bufio.NewReader(bytes.NewReader(i.raw))
-	b, err := r.ReadByte()
+	// fmt.Println(n)
+	// fmt.Println(buf)
+	// fmt.Println(string(buf[:n]))
+	// fmt.Println(strings.TrimPrefix(string(buf[:n]), "goroutine "))
+
+	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
+
+	// fmt.Println("w", idField)
+
+	id, err := strconv.Atoi(idField)
+
 	if err != nil {
-		log.Fatal("error reading 1st byte: ", err)
-	}
-	switch b {
-	case Array:
-		cmds, err := decodeArray(r)
-		if err != nil {
-			log.Fatal("error parsing input stream: ", err)
-		}
-		i.cmds = cmds
+		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
 	}
 
+	return id
+
 }
-func mustCopy(dst io.Writer, src io.Reader) {
-	if _, err := io.Copy(dst, src); err != nil {
+
+func handleRequest(conn net.Conn) {
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+
+	if err != nil {
 		log.Fatal(err)
 	}
-}
-func handleConn(conn net.Conn) {
-	defer conn.Close()
-	for {
-		barr := make([]byte, 1024)
-		_, err := conn.Read(barr)
 
-		if err == io.EOF {
-			log.Println("client is done")
-			return
-		}
-		if err != nil {
-			
-			log.Fatal(err)
-		}
-		in := input{raw: barr}
-		in.parse()
-		switch strings.ToUpper(in.cmds[0]) {
-		case "PING":
-			mustCopy(conn, strings.NewReader("+PONG\r\n"))
-		case "ECHO":
-
-			mustCopy(conn, strings.NewReader(fmt.Sprintf("%s%s%s", "+", in.cmds[1], "\r\n")))
-		default:
-			mustCopy(conn, strings.NewReader("+PONG\r\n"))
-		}
+	if n != 0 {
+		fmt.Println("\nreceived", string(buffer[:n]))
 	}
+
+	conn.Write([]byte("xnxx"))
+	fmt.Println("xoxo")
+	fmt.Println("go: ", goid())
 }
+
 func main() {
-	l, err := net.Listen("tcp", "localhost:6379")
+
+	fmt.Println("Logs from your program will appear here!")
+
+	// Uncomment this block to pass the first stage
+
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	} else {
+		fmt.Println("listening...")
 	}
+
+	defer l.Close()
+
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			log.Println(err)
-			continue
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
-		go handleConn(conn)
+
+		go handleRequest(conn)
 	}
+
 }
